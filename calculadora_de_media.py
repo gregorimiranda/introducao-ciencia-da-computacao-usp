632c09bc773cd55944dabf417aac95401bbcae05
gregorimiranda <gregorimirandaoficial@gmail.com>
2023-08-31 22:47:13 -0300

    Update calculadora_de_media.py

--- calculadora_de_media.py
+++ calculadora_de_media.py
@@ -16,10 +16,15 @@
 
 # A média aritmética é 5.5
 
+# Inputando as notas 
+
 nota1 = int(input("Digite a primeira nota"))
 nota2 = int(input("Digite a segunda nota"))
 nota3 = int(input("Digite a terceira nota"))
 nota4 = int(input("Digite a quarta nota"))
 
+# Calculando a média
 media = (nota1 + nota2 + nota3 + nota4) / 4
+
+#Imprimindo o resultado
 print("A média aritmética é",media)
